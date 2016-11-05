3562473fc0f10c3a931fc145993f251686a38e71
steady-daddy <thisismansoorkhan@gmail.com>
2016-11-05 13:06:18 +0530

    app setup with artistlist

---
+++ README.md
@@ -0,0 +1,11 @@
+# AngularJS2 - Basic Search and Filter Sample App
+This project is a sample AngularJS2 app to search and filter records. It uses AngularJS2, TypeScript, Sass, Compass, Susy 2.0 and shows a variety of techniques including dockable menus, responsive grids, tween and parallax effects, etc.
+
+## Instructions
+ - Clone the repository: `git clone https://github.com/steady-daddy/angular2-search-filter`
+ - Run `npm install` to install dependencies
+ - Run `gulp`
+ - Fire up your browser at: `http://localhost:8000`
+
+### This project is based on the course Up and Running with AngularJS2: The Basics tutorial by Ray Villalobos of LinkedIn. https://github.com/planetoftheweb/angular2
+
--- gulpfile.js
+++ gulpfile.js
@@ -2,30 +2,53 @@ var gulp = require('gulp'),
     webserver = require('gulp-webserver'),
     typescript = require('gulp-typescript'),
     sourcemaps = require('gulp-sourcemaps'),
-    tscConfig = require('./tsconfig.json');
+    tscConfig = require('./tsconfig.json')
+    gutil = require('gulp-util'),
+    browserify = require('gulp-browserify'),
+    compass = require('gulp-compass'),
+    connect = require('gulp-connect'),
+    gulpif = require('gulp-if'),
+    uglify = require('gulp-uglify'),
+    minifyHTML = require('gulp-minify-html'),
+    concat = require('gulp-concat');
+    path = require('path');
 
-var appSrc = 'builds/development/',
-    tsSrc = 'process/typescript/';
 
-gulp.task('html', function() {
-  gulp.src(appSrc + '**/*.html');
-});
+var env,
+    jsSources,
+    sassSources,
+    htmlSources,
+    outputDir,
+    sassStyle,
+    tsSrc;
 
-gulp.task('css', function() {
-  gulp.src(appSrc + '**/*.css');
-});
+env = 'development';
+
+if (env==='development') {
+    outputDir = 'builds/development/';
+    sassStyle = 'expanded';
+} else {
+    outputDir = 'builds/production/';
+    sassStyle = 'compressed';
+}
+
+tsSrc = 'components/scripts/';
+sassSources = ['components/sass/style.scss'];
+htmlSources = [outputDir + '*.html'];
+
+jsSources = [
+    'node_modules/es6-shim/es6-shim.min.js',
+    'node_modules/systemjs/dist/system-polyfills.js',
+    'node_modules/angular2/bundles/angular2-polyfills.js',
+    'node_modules/systemjs/dist/system.src.js',
+    'node_modules/rxjs/bundles/Rx.js',
+    'node_modules/angular2/bundles/angular2.dev.js'
+];
 
 gulp.task('copylibs', function() {
   return gulp
-    .src([
-      'node_modules/es6-shim/es6-shim.min.js',
-      'node_modules/systemjs/dist/system-polyfills.js',
-      'node_modules/angular2/bundles/angular2-polyfills.js',
-      'node_modules/systemjs/dist/system.src.js',
-      'node_modules/rxjs/bundles/Rx.js',
-      'node_modules/angular2/bundles/angular2.dev.js'
-    ])
-    .pipe(gulp.dest(appSrc + 'js/lib/angular2'));
+    .src(jsSources)
+    .pipe(gulp.dest(outputDir + 'js/lib/angular2'));
 });
 
 gulp.task('typescript', function () {
@@ -34,21 +57,53 @@ gulp.task('typescript', function () {
     .pipe(sourcemaps.init())
     .pipe(typescript(tscConfig.compilerOptions))
     .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest(appSrc + 'js/'));
+    .pipe(gulp.dest(outputDir + 'js/'));
+});
+
+//gulp.task('js', function() {
+//    gulp.src(jsSources)
+//        .pipe(concat('script.js'))
+//        .pipe(browserify())
+//        .on('error', gutil.log)
+//        .pipe(gulpif(env === 'production', uglify()))
+//        .pipe(gulp.dest(outputDir + 'js'))
+//        .pipe(connect.reload())
+//});
+
+gulp.task('compass', function() {
+    gulp.src(sassSources)
+        .pipe(compass({
+            sass: 'components/sass',
+            css: outputDir + 'css',
+            image: outputDir + 'images',
+            style: sassStyle,
+            require: ['susy', 'breakpoint']
+        })
+            .on('error', gutil.log))
+        //    .pipe(gulp.dest( outputDir + 'css'))
+        .pipe(connect.reload())
+});
+
+
+gulp.task('html', function() {
+    gulp.src('builds/development/*.html')
+        .pipe(gulpif(env === 'production', minifyHTML()))
+        .pipe(gulpif(env === 'production', gulp.dest(outputDir)))
+        .pipe(connect.reload())
 });
 
 gulp.task('watch', function() {
   gulp.watch(tsSrc + '**/*.ts', ['typescript']);
-  gulp.watch(appSrc + 'css/*.css', ['css']);
-  gulp.watch(appSrc + '**/*.html', ['html']);
+  gulp.watch(['components/sass/*.scss', 'components/sass/*/*.scss'], ['compass']);
+  gulp.watch(outputDir + '**/*.html', ['html']);
 });
 
 gulp.task('webserver', function() {
-  gulp.src(appSrc)
+  gulp.src(outputDir)
     .pipe(webserver({
       livereload: true,
-      open: true
+      open: false
     }));
 });
 
-gulp.task('default', ['copylibs', 'typescript', 'watch', 'webserver']);
+gulp.task('default', ['copylibs', 'typescript', 'html','compass', 'watch', 'webserver']);
