eb6d3c4bc9171e989254258acb8a0b9afe0527c0
steady-daddy <thisismansoorkhan@gmail.com>
2016-11-05 14:52:57 +0530

    Adding inline styles

--- components/scripts/custom/app.component.ts
+++ components/scripts/custom/app.component.ts
@@ -2,7 +2,35 @@ import {Component} from 'angular2/core';
 
 @Component({
     selector: 'my-app',
-    templateUrl: 'partials/app.html'
+    templateUrl: 'partials/app.html',
+    styles: [
+        `.btn {
+            display: inline-block;
+            padding: 12px 24px;
+            margin-bottom: 0;
+            font-size: 1.3rem;
+            line-height: 140%;
+            text-align: center;
+            text-decoration: none;
+            white-space: nowrap;
+            vertical-align: middle;
+            cursor: pointer;
+            border: 1px solid transparent;
+            border-radius: 4px;
+            color: white;
+            background-color: #2D8BCF;
+            background-size: 18px 18px;
+            min-width: 28px;
+            min-height: 28px;
+            background-position: center center;
+            background-repeat: no-repeat;
+            -webkit-user-select: none;
+               -moz-user-select: none;
+                -ms-user-select: none;
+                 -o-user-select: none;
+                    user-select: none;
+        }`
+    ]
 })
 
 export class AppComponent{
